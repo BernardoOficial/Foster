05b0dd3b4aad29301f29ad0ccb80a0709ecff332
Bernardo <bernardo.258@hotmail.com>
2020-07-26 15:06:18 -0300

    Colocando o projeto - Foster no Github

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Função para abrir e fechar menu 
+// O método toggle vai ficar alternando uma classe
+function btnMenuToggle() {
+    document
+        .querySelector('.secaoMenu div')
+        .classList
+        .toggle('toggle')
+}
